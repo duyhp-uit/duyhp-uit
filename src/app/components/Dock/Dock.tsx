ad5f6e9d8f086f84cb352dab064c708a4da35119
Duy Huynh <kudophuongduy@gmail.com>
2025-05-19 20:51:33 +0700

    Initial commit.

---
+++ src/app/components/Dock/Dock.tsx
@@ -0,0 +1,12 @@
+const Dock = () => {
+  return (
+    <div
+      className="flex items-center fixed left-1/2 -translate-x-1/2 h-[65px] p-4
+     rounded-[14px] border-[1px] border-solid border-[#ffffff33] backdrop-blur-[20px] bottom-[3%]"
+    >
+      <div>ádasdasdasdasdasdasdsd</div>
+    </div>
+  );
+};
+
+export default Dock;
---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="w-screen top-0 left-0 py-1 px-1 flex gap-1">
+      <ul className="font-bold text-white">File</ul>
+      <ul className="font-bold text-white">Edit</ul>
+      <ul className="font-bold text-white">View</ul>
+      <ul className="font-bold text-white">Go</ul>
+      <ul className="font-bold text-white">Tools</ul>
+      <ul className="font-bold text-white">Help</ul>
+    </div>
+  );
+};
+export default Navbar;
